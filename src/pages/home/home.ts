3126cb77b689aeedd83d14d74c0f8a1a008c08f0
wunderdoge <services@alkl.de>
2018-02-18 18:04:10 +0100

    basic layout

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { NativeStorage } from '@ionic-native/native-storage';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,83 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  currentDate = Date.now();
+  lastDate = 0;
+
+  constructor(private nativeStorage: NativeStorage, public navCtrl: NavController, public alertCtrl: AlertController) {
 
   }
+
+  ionViewDidLoad() {
+    this.nativeStorage.getItem('croqueEvents')
+      .then(
+        data => currentDate = data.lastTimestamp,
+        error => {
+          let alert = this.alertCtrl.create({
+            title: 'Error',
+            subTitle: "Can't read data: " + error,
+            buttons: ['OK']
+          });
+          alert.present();
+        }
+      );
+  }
+
+  doConfirm() {
+    let confirm = this.alertCtrl.create({
+      title: 'Eat Croque?',
+      message: 'Srsly?',
+      buttons: [
+        {
+          text: 'No',
+          handler: () => {}
+        },
+        {
+          text: 'Yes',
+          handler: () => {
+            let now = Date.now();
+            this.nativeStorage.setItem('croqueEvents', {lastTimestamp: now})
+              .then(
+                () => this.lastDate = now,
+                error => {
+                  let alert = this.alertCtrl.create({
+                    title: "Error",
+                    subTitle: "Can't store data: " + error,
+                    buttons: ['OK']
+                  });
+                  alert.present();
+                }
+              );
+          }
+        }
+      ]
+    });
+
+    confirm.present()
+  }
+
+  doDebug() {
+    this.nativeStorage.getItem('croqueEvents')
+      .then(
+        data => {
+          let alert = this.alertCtrl.create({
+            title: 'Success',
+            subTitle: data.lastTimestamp,
+            buttons: ['OK']
+          });
+          alert.present();
+        },
+        error => {
+          let alert = this.alertCtrl.create({
+            title: 'Error',
+            subTitle: "Can't read data: " + error,
+            buttons: ['OK']
+          });
+          alert.present();
+        }
+      );
+
+  }
+
 
 }
